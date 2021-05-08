c0efee3aed655cdaf3961df8ddb86d1c8950765e
deJonge-Lars <dejonge.lars.webdeveloper@gmail.com>
2021-05-08 21:45:25 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8ec0c0a1-e934-4944-826d-95cde76616dd';
+export const MODAL_CLOSE_SEC = 3;
